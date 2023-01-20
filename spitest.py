905445e3fa65ccbe51c755e4c02e22cd1e9c859e
Akash Levy <akashlevy@gmail.com>
2023-01-20 13:28:47 -0800

    Updates

--- spitest.py
+++ spitest.py
@@ -1,10 +1,20 @@
+# Import spidev library
 from spidev import SpiDev
 
+# Create and configure SPI interface
 spi = SpiDev()
 spi.open(0,0)
 spi.cshigh = True
 spi.max_speed_hz = 4000
+
+# Message to run a READ message by writing to the CMD register
 msg = [0xd8] + [0x00]*19 + [0x04]
-msg = [0xc7] + [0x00]*20
+
+# Message to read from register 31 (should read out ASCII "RAM")
+msg = [0x7c] + [0x00]*20
+
+# Transfer message and print bytes received on mosi
 print(spi.xfer(msg))
+
+# Close SPI interface
 spi.close()
