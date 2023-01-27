55114b81491b0ba9a752796617e0619e25220208
Akash Levy <akashlevy@gmail.com>
2023-01-27 00:32:04 -0800

    Commit formed

--- form_chip.py
+++ form_chip.py
@@ -6,7 +6,6 @@ from ember import EMBERDriver
 parser = argparse.ArgumentParser(description="FORM a chip.")
 parser.add_argument("chipname", help="chip name for logging")
 parser.add_argument("--config", type=str, default="config/form.json", help="config file")
-parser.add_argument("--outfile", type=str, default="log/form.json", help="file to output to")
 parser.add_argument("--start-addr", type=int, default=0, help="start address")
 parser.add_argument("--end-addr", type=int, default=65536, help="end address")
 parser.add_argument("--step-addr", type=int, default=1, help="address stride")
