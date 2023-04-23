0415d345c4d4c4536ffc0a2e280ea9ae695d8ce6
Akash Levy <akashlevy@gmail.com>
2023-04-23 10:16:05 -0700

    4bpc CB

--- data/process.py
+++ data/process.py
@@ -1,5 +1,5 @@
-lines = open('cb3bpc.csv').readlines()
-with open('3bpc.csv', 'w') as outfile:
+lines = open('cb4bpc.csv').readlines()
+with open('4bpc.csv', 'w') as outfile:
     for i in range(3):
         for j in range(144*i + 48, 144*(i+1)):
             lines[144*i + j%48] = lines[144*i + j%48].replace('\n', '\t' + '\t'.join(lines[j].split('\t')[2:]))
