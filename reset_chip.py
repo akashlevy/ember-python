cfb8d9b43cbf56390c5f30c72ed1a069eead418e
Akash Levy <akashlevy@gmail.com>
2023-06-12 22:02:15 -0700

    Add native, fast, slow mode for RESET chip

--- reset_chip.py
+++ reset_chip.py
@@ -9,38 +9,48 @@ parser.add_argument("--config", type=str, default="settings/form.json", help="co
 parser.add_argument("--start-addr", type=int, default=0, help="start address")
 parser.add_argument("--end-addr", type=int, default=65536, help="end address")
 parser.add_argument("--step-addr", type=int, default=1, help="address stride")
+parser.add_argument("--native", action="store_true", help="use native RESET-verify")
+parser.add_argument("--fast", action="store_true", help="no RESET-verify")
+parser.add_argument("--superfast", action="store_true", help="no RESET-verify at full clock speed")
 parser.add_argument("--debug", action="store_true", help="enable debugging")
 args = parser.parse_args()
 
 # Initialize EMBER system
 with EMBERDriver(args.chipname, args.config) as ember:
-  # Reduce max attempts, ignore failures, reduce SET PW
-  ember.settings["max_attempts"] = 100
-  ember.settings["ignore_failures"] = 1
-  ember.settings["pw_set_cycle_exp"] = 0
-  ember.settings["pw_set_cycle_mantissa"] = 1
-  ember.commit_settings()
-  
-  # # Do READ operation across cells
-  # prereads = []
-  # for addr in range(args.start_addr, args.end_addr, args.step_addr):
-  #   ember.set_addr(addr)
-  #   prereads.append(ember.read())
-  # print("PREREAD:")
-  # for read in prereads:
-  #   print(read)
+  # Native mode
+  if args.native:
+    ember.set_addr(args.start_addr, args.end_addr-1, args.step_addr)
+    if args.fast or args.superfast:
+      ember.write(0, use_multi_addrs=True)
+      if args.superfast:
+        ember.fast_mode()
+      ember.wait_for_idle()
+    else:
+      # Do operation across cells
+      for addr in range(args.start_addr, args.end_addr, args.step_addr):
+        ember.set_addr(addr)
+        ember.write(0, debug=args.debug)
+        print("Address", addr, "DONE:", ember.read())
 
-  # Do operation across cells
-  for addr in range(args.start_addr, args.end_addr, args.step_addr):
-    ember.set_addr(addr)
-    ember.write(0, debug=args.debug)
-    print("Address", addr, "DONE:", ember.read())
+  # Fast mode
+  elif args.fast or args.superfast:
+    ember.set_addr(args.start_addr, args.end_addr-1, args.step_addr)
+    for i in range(8,30):
+      ember.reset_pulse(use_multi_addrs=True, mask=1<<i)
+      if args.superfast:
+        ember.fast_mode()
+      ember.wait_for_idle()
+  # Slow mode
+  else:
+    # Reduce max attempts, ignore failures, reduce SET PW
+    ember.settings["max_attempts"] = 5
+    ember.settings["ignore_failures"] = 1
+    ember.settings["pw_set_cycle_exp"] = 0
+    ember.settings["pw_set_cycle_mantissa"] = 1
+    ember.commit_settings()
 
-  # # Do READ operation across cells
-  # postreads = []
-  # for addr in range(args.start_addr, args.end_addr, args.step_addr):
-  #   ember.set_addr(addr)
-  #   postreads.append(ember.read())
-  # print("POSTREAD:")
-  # for read in postreads:
-  #   print(read)
+    # Do operation across cells
+    for addr in range(args.start_addr, args.end_addr, args.step_addr):
+      ember.set_addr(addr)
+      ember.write(0, native=False, debug=args.debug)
+      print("Address", addr, "DONE:", ember.read())
