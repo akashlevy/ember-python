da89496b0845bfcba155e1ca09f507ed0f528440
Akash Levy <akashlevy@gmail.com>
2023-10-27 11:42:57 -0700

    Big updates

---
+++ sweep_script.sh
@@ -0,0 +1,4 @@
+python sweep_chip.py CHIP9 data/sweep/setsweep33_newest2.csv --shuffle
+python sweep_chip.py CHIP9 data/sweep/resetsweep33_newest.csv --shuffle --reset
+python sweep_chip.py CHIP9 data/sweep/resetsweep33_newest.csv --shuffle --reset
+python retention_chip.py CHIP9 --retention-outfile data/retention/retention6.csv --fast
