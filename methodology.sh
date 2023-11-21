0f2fdbaaaeec8c3e6a7bb62cd6057cc1967501b4
Akash Levy <akashlevy@gmail.com>
2023-11-21 09:12:17 -0800

    Clean up

--- methodology.sh
+++ methodology.sh
@@ -1,14 +1,15 @@
-python form_chip.py CHIP_N --native --superfast
-python read_chip.py CHIP_N form.csv
-python reset_chip.py CHIP_N --native --superfast
-python read_chip.py CHIP_N reset.csv
+CHIP_N=CHIP_2
+python form_chip.py $CHIP_N --native
+python read_chip.py $CHIP_N form.csv
+python reset_chip.py $CHIP_N --native --superfast
+python read_chip.py $CHIP_N reset.csv
 
-python sweep_chip.py CHIP_N setsweep33.csv --shuffle
-python sweep_chip.py CHIP_N setsweep33.csv --shuffle
-python sweep_chip.py CHIP_N resetsweep33.csv --shuffle --reset
-python sweep_chip.py CHIP_N resetsweep33.csv --shuffle --reset
+python sweep_chip.py $CHIP_N setsweep33.csv --shuffle
+python sweep_chip.py $CHIP_N setsweep33.csv --shuffle
+python sweep_chip.py $CHIP_N resetsweep33.csv --shuffle --reset
+python sweep_chip.py $CHIP_N resetsweep33.csv --shuffle --reset
 
-python retention_chip.py CHIP_N retention.csv --fast --shuffle --ret-reads 1
+python retention_chip.py $CHIP_N retention.csv --fast --shuffle --ret-reads 1
 
 # Process read retention
 
